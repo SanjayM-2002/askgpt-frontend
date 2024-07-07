8d5ca0cec621e9dd910086c4b1617bce9344fe19
SanjayM-2002 <sanjaymshivakripa@gmail.com>
2024-07-07 06:24:18 +0530

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,25 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Chat from './pages/Chat';
+import InvalidPage from './pages/InvalidPage';
 
 function App() {
   return (
     <>
-      <div>Hello world</div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='*' element={<InvalidPage />} />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'serif',
+    allVariants: { color: 'white' },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
